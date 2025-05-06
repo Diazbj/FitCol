6438b22f4815670d8c2f7db6e527c481cf68eef8
Jordy Diaz <diaz.jordyb@gmail.com>
2025-05-05 19:31:28 -0500

    Crud Cliente mapper

--- src/main/java/co/edu/uniquindio/proyecto/controladores/EntrenadorControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/EntrenadorControlador.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/entrenador")
+@RequestMapping("/api/entrenadores")
 public class EntrenadorControlador {
 
     private final EntrenadorServicio entrenadorServicio;
--- src/main/java/co/edu/uniquindio/proyecto/controladores/NutricionistaControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/NutricionistaControlador.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/nutricionista")
+@RequestMapping("/api/nutricionistas")
 public class NutricionistaControlador {
 
     private final NutricionistaServicio nutricionistaServicio;
