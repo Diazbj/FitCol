48122e6d662011f91907fe4a21bc8d20018332fe
Jordy Diaz <diaz.jordyb@gmail.com>
2025-05-06 21:40:13 -0500

    Crud Cliente mapper

--- src/main/java/co/edu/uniquindio/proyecto/controladores/CiudadControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/CiudadControlador.java
@@ -1,8 +1,10 @@
 package co.edu.uniquindio.proyecto.controladores;
 
 import co.edu.uniquindio.proyecto.dto.CiudadDTO;
+import co.edu.uniquindio.proyecto.dto.MensajeDTO;
 import co.edu.uniquindio.proyecto.modelo.vo.Ciudad;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import co.edu.uniquindio.proyecto.repositorio.CiudadRepo;
@@ -20,11 +22,13 @@ public class CiudadControlador {
     private final CiudadRepo ciudadRepo;
 
     @GetMapping
-    public List<CiudadDTO> obtenerCiudades() {
+    public ResponseEntity<MensajeDTO<List<CiudadDTO>>> obtenerCiudades() {
         List<Ciudad> ciudades = ciudadRepo.findAll();
-        return ciudades.stream()
+        List<CiudadDTO> respuesta = ciudades.stream()
                 .map(ciudad -> new CiudadDTO(ciudad.getId(), ciudad.getNombre()))
                 .toList();
+
+        return ResponseEntity.ok(new MensajeDTO<>(false, respuesta));
     }
 }
 
